007cae67b54e06cbe79ec278c5234f435e25bb64
Amin Aslami <101183453+aminaslami@users.noreply.github.com>
2026-06-10 18:38:53 +0300

    Deneylerde SEED = 42 sabit tohum değeri kull.

---
+++ Seed_Tohum(42).py
@@ -0,0 +1,9 @@
+import torch, numpy as np, random
+from transformers import set_seed
+
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+set_seed(SEED)
